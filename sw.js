c479914141d444dc13439fc73ee1f5299f06d891
rrajr <158168217+rrajr@users.noreply.github.com>
2026-05-29 22:34:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,48 @@
-self.addEventListener('fetch', function(event){});
+const cacheName = 'cache-rrajr-v1';
+const cacheResources = [
+	'index.html', 
+];
+async function precache(){
+	const cache = await caches.open(cacheName);
+	return cache.addAll(cacheResources);
+}
+self.addEventListener("install", event => {
+	event.waitUntil(precache());
+});
+async function networkFirst(request) {
+	try {
+		const networkResponse = await fetch(request);
+		if (networkResponse.ok) {
+			const cache = await caches.open(cacheName);
+			cache.put(request, networkResponse.clone());
+		}
+		return networkResponse;
+	} catch (error) {
+		const cachedResponse = await caches.match(request);
+		return cachedResponse || Response.error();
+	}
+}
+async function cacheFirst(request) {
+	const cachedResponse = await caches.match(request);
+	if (cachedResponse) {
+		return cachedResponse;
+	}
+	try {
+		const networkResponse = await fetch(request);
+		if (networkResponse.ok) {
+			const cache = await caches.open("cache-rrajr-v1");
+			cache.put(request, networkResponse.clone());
+		}
+		return networkResponse;
+	} catch (error) {
+		return Response.error();
+	}
+}
+self.addEventListener("fetch", (event) => {
+	const url = new URL(event.request.url);
+	if (url.pathname.match(/^\/webfonts/)) {
+		event.respondWith(cacheFirst(event.request));
+	} else {
+		event.respondWith(networkFirst(event.request));
+	}
+});
